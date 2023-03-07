0ef27bd7b042ff2c40e541236aef29768fbe9620
Marleess <marleysumarly@outlook.com>
2023-03-07 19:18:52 +0700

    Update table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,8 @@ class CreateUsersTable extends Migration
             $table->string('email_validate')->nullable();
             $table->enum('status', ['active', 'non-active'])->default('non-active');
             $table->dateTime('last_login')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
