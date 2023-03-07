c11f9e4b5e1fb2374c73803546b817149f28577d
Marleess <marleysumarly@outlook.com>
2023-03-07 23:55:12 +0700

    Create API

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +23,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'email_validate',
+        'status',
+        'last_login'
+
     ];
 
     /**
@@ -41,4 +48,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+     $this->attributes['password'] = bcrypt($password);
+    }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+           'role' => 'admin',
+            'password' => 'admin',
+            'status' => 'active',
+            'last_login' => now()
+        ]);
+
+        User::factory()->count(50)->create();
+    }
+}
